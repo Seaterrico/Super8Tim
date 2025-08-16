74d653b54992358dc451383bb6c39093fee6bb11
scou766 <107221062+Seaterrico@users.noreply.github.com>
2025-08-16 18:13:05 +1200

    Fix

--- scripts/headerFooterLoaders.js
+++ scripts/headerFooterLoaders.js
@@ -1,5 +1,7 @@
+const base = window.location.pathname.includes("/pages/") ? ".." : ".";
+
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("components/header.html")
+    fetch(`${base}/components/header.html`)
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-container").innerHTML = data;
@@ -7,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("components/footer.html")
+    fetch(`${base}/components/footer.html`)
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-container").innerHTML = data;
